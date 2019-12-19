afc131580d4458ada674d082c3df4946e07e8a30
KoreaCodingKing <50541727+KoreaCodingKing@users.noreply.github.com>
2019-12-19 13:40:28 +0900

    clonecoding
    language: Nodejs
    database; firebase(Cloud noSQL)
    
    https://www.youtube.com/watch?v=m_u6P5k0vP0&t=6526s

---
+++ socialapp-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCoSsx8wT89DT1stYA8FM0gL3c9A15tOeE",
+  authDomain: "socialapp-ee961.firebaseapp.com",
+  databaseURL: "https://socialapp-ee961.firebaseio.com",
+  projectId: "socialapp-ee961",
+  storageBucket: "socialapp-ee961.appspot.com",
+  messagingSenderId: "615066700459",
+  appId: "1:615066700459:web:5bdc37830ff2d6a70505ee",
+  measurementId: "G-6Y3ZPRLYP2"
+};
